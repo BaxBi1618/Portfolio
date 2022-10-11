b7e27cb440ebf8bd2b17d88d7be88a1746235f5e
BaxBi1618 <m.programista@wp.pl>
2022-10-11 22:04:53 +0200

    Adds portfolio #2
    Dodanie imienia oraz wystylizowanie boksów z social media

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("my_name").onmouseover = function(){
-    document.getElementById("my_name").style.animationName = "name-zoom-in";
+    document.getElementById("my_name").style.backgroundColor = "#e5383b"
 }
 document.getElementById("my_name").onmouseout = function(){
-    document.getElementById("my_name").style.animationName = "name-zoom-out";
+    document.getElementById("my_name").style.backgroundColor = "#131313"
 }
