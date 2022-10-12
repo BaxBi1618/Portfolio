11c355253cf8b6bf184581e7299fc04610920085
BaxBi1618 <m.programista@wp.pl>
2022-10-12 19:55:08 +0200

    Adds portfolio#4
    dodanie boksu o mnie oraz zdjęcia

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ document.getElementById("my_name").onmouseover = function(){
 document.getElementById("my_name").onmouseout = function(){
     document.getElementById("my_name").style.backgroundColor = "#131313"
 }
+
+$("#my-photo").mouseover(function(){
+    $("#my-photo").css("background-image","url(image/me1.jpg)");
+});
+$("#my-photo").mouseout(function(){
+    $("#my-photo").css("background-image","url(image/me.jpg)");
+});
