afb6c9ee4ac302256fbd5b187f30dccbe1aa2f7a
BaxBi1618 <m.programista@wp.pl>
2022-10-15 11:37:07 +0200

    Adds portfolio#7

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ $("#number-2").mouseover(function(){
 $("#number-2").mouseout(function(){
     $("#number-2").css("background-image","url(image/Desk1.jpg)");
 });
+
+let button_state = 0;
+$("#more-about-me").click(function(){
+    if(button_state == 0){
+        $("#more-about-me").html("LESS ABOUT ME");
+        button_state ++;
+    }
+    else if(button_state == 1){
+        $("#more-about-me").html("MORE ABOUT ME");
+        button_state --;
+    }
+});
