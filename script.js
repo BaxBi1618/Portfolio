311fa3be071db841c87f4f22cdb7b161c2b75405
BaxBi1618 <93683656+BaxBi1618@users.noreply.github.com>
2023-03-02 14:54:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -15,11 +15,24 @@ $("#number-2").mouseout(function(){
 let button_state = 0;
 $("#more-about-me").click(function(){
     if(button_state == 0){
-        $("#more-about-me").text("LESS ABOUT ME")
+        $("#more-about-me").text("LESS ABOUT ME");
+        $("#about-me").css("width","100%");
+        $("#about-me").css("min-width","1266px");
+        $("#about-me").css("margin","40px 55px 0 35px");
+        $("#side-div").css("width","100%");
+        $("#side-div").css("min-width","1338px");
+        $("#main-div").css("flex-wrap","wrap");
         button_state ++;
     }
     else if(button_state == 1){
         $("#more-about-me").html("MORE ABOUT ME");
+        $("#about-me").css("width","26%");
+        $("#about-me").css("min-width","508px");
+        $("#about-me").css("margin","40px 20px 0 20px");
+        $("#side-div").css("width","70%");
+        $("#side-div").css("min-width","820px");
+        $("#main-div").css("flex-wrap","nowrap");
+        
         button_state --;
     }
     $("#div-more-about").fadeToggle(0);
